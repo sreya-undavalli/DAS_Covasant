0d32b0df678ac99ea6edf35bfcf346c2299d11c2
sreya-undavalli <122337255+sreya-undavalli@users.noreply.github.com>
2025-04-10 23:48:54 +0530

    Day3.py

--- Day3.py
+++ Day3.py
@@ -14,3 +14,17 @@ def flatten(lst):
 
 lst = [1,2,3, [1,2,3,[3,4],2]]
 print(flatten(lst))
+
+
+DAY 3 -Q2
+lst = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
+def convert (lst):
+    for item in lst:
+        if type(item) is list:
+            convert(item)
+             
+        else:
+            lst[lst.index(item)]=[int(x) for x in (item[1:-1:2])]
+    return lst
+             
+print(convert(lst))
