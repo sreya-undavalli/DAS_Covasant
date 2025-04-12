cabd592a6637a6552b56f653f2ad05616f528458
sreya-undavalli <122337255+sreya-undavalli@users.noreply.github.com>
2025-04-12 13:48:33 +0530

    file.py

--- Day4.py/pkg/file.py
+++ Day4.py/pkg/file.py
@@ -1,1 +1,27 @@
+import os
+import datetime
 
+class File:
+    def __init__(self, directory):
+        self.directory = directory
+
+    def getMaxSizeFile(self, n):
+        files = [f for f in os.listdir(self.directory)
+                 if os.path.isfile(os.path.join(self.directory, f))]
+        files_sorted = sorted(
+            files,
+            key=lambda f: os.path.getsize(os.path.join(self.directory, f)),
+            reverse=True
+        )
+        return files_sorted[:n]
+
+    def getLatestFiles(self, date_obj):
+        latest_files = []
+        for f in os.listdir(self.directory):
+            full_path = os.path.join(self.directory, f)
+            if os.path.isfile(full_path):
+                mod_time = os.path.getmtime(full_path)
+                mod_date = datetime.date.fromtimestamp(mod_time)
+                if mod_date > date_obj:
+                    latest_files.append(f)
+        return latest_files
