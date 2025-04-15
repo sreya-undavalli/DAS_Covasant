64763f3b1f90e5ddfd0c26557167101ae3ef28ab
sreya-undavalli <122337255+sreya-undavalli@users.noreply.github.com>
2025-04-15 10:59:38 +0530

    Day1.py

--- Day1.py
+++ Day1.py
@@ -1,21 +1,30 @@
-# Given dictionaries
 D1 = {'ok': 1, 'nok': 2}
-D2 = {'ok': 2, 'new': 3}
+D2 = {'ok': 2, 'new':3 }
 
-# Union of keys (value does not matter)
-D_UNION = {**D1, **D2}
+D_UNION={**D1,**D2}
+print(f"D_UNION: {D_UNION}")
 
-# Intersection of keys (value does not matter)
-D_INTERSECTION = {key: D1[key] for key in D1 if key in D2}
+D_INTERSECTION={}
+for key in D1:
+    if key in D2:
+        D_INTERSECTION[key]=D1[key]
+print(f"D_INTERSECTION: {D_INTERSECTION}")
 
-# D1 - D2 (keys in D1 but not in D2)
-D1_minus_D2 = {key: D1[key] for key in D1 if key not in D2}
+D1D2={}
+for key in D1:
+    if key not in D2:
+        D1D2[key]=D1[key]
+print(f"D1-D2: {D1D2}")
 
-# Merge values for same keys (add values for same keys)
-D_MERGE = {key: D1.get(key, 0) + D2.get(key, 0) for key in set(D1) | set(D2)}
 
-# Printing the results
-print("D_UNION:", D_UNION)
-print("D_INTERSECTION:", D_INTERSECTION)
-print("D1 - D2:", D1_minus_D2)
-print("D_MERGE:", D_MERGE)
+D_MERGE={}
+for key in D1:
+    if key in D2:
+        D_MERGE[key]=D1[key]+D2[key]
+    else:
+        D_MERGE[key]=D1[key]
+for key in D2:
+    if key not in D_MERGE:
+        D_MERGE[key]=D2[key]
+print(f"D_MERGE: {D_MERGE}")
+         
